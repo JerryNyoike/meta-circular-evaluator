49b0f9d38ebefade3f601e7c001c815d377f5e17
JerryNyoike <jngugi31@gmail.com>
2021-01-18 18:02:26 +0300

    Add procedure for evaluating sequences

--- eval.scm
+++ eval.scm
@@ -30,3 +30,10 @@
   (if (true? (eval (if-predicate exp) env))
     (eval (if-consequent exp) env)
     (eval (if-alternative exp) env)))
+
+(define (eval-sequence exps env)
+  (cond ((last-exp? exps)
+	 (eval (first-exp exps) env))
+	(else
+	  (eval (first-exp exps) env)
+	  (eval (rest-exps exps) env))))
