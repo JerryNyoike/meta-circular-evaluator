3a0138d26516e28e331f6429d8c5147281d8a7b6
JerryNyoike <jngugi31@gmail.com>
2021-02-18 10:45:42 +0300

    Add procedures to enable primitives to be applied in the implementing language

--- apply.scm
+++ apply.scm
@@ -1,5 +1,12 @@
 (declare (usual-integrations))
 
+(define apply-in-underlying-scheme apply)
+
+(define (apply-primitive-procedure proc args)
+  (apply-in-underlying-scheme
+    (primitive-implementation proc)
+    args))
+
 (define (apply procedure arguments)
   (cond ((primitive-procedure? procedure)
 	 (apply-primitive-procedure procdeure arguments))
