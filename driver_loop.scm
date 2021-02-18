babc797bd65c01d3b1990fbd8a0fdce9b7b04c50
JerryNyoike <jngugi31@gmail.com>
2021-02-18 10:51:00 +0300

    Add an input output loop that mimics the REPL

---
+++ driver_loop.scm
@@ -0,0 +1,28 @@
+(declare (usual-integrations))
+
+(load "eval.scm")
+
+(define input-prompt  ";;; M-Eval input:")
+(define output-prompt ";;; M-Eval value")
+
+(define (prompt-for-input string)
+  (newline) (newline) (display string) (newline))
+(define (announce-output string)
+  (newline) (display string) (newline))
+(define (user-print object)
+  (if (compound-procedure? object)
+    (display (list 'compound-procedure
+		   (procedure-parameters object)
+		   (procedure-body object)
+		   '<procedure-env>))
+    (display object)))
+
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input (read)))
+    (let ((output (eval input the-global-environment)))
+      (announce-output output-prompt)
+      (user-print output)))
+    (driver-loop))
+
+(driver-loop)
